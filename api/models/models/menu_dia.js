63ddda69f3e9403e7e682aaf893bb20a4b985cce
valeri <valeritsert@gmail.com>
2019-02-13 16:26:23 +0400

    fix primary key for menu_dia, plan_semanal

--- api/models/models/menu_dia.js
+++ api/models/models/menu_dia.js
@@ -4,8 +4,8 @@ module.exports = function (sequelize, DataTypes) {
   return sequelize.define('menu_dia', {
     id: {
       type: DataTypes.INTEGER(11),
-      allowNull: false,
       primaryKey: true,
+      autoIncrement: true,
     },
     nombre: {
       type: DataTypes.STRING(45),
--- api/models/models/plan_semanal.js
+++ api/models/models/plan_semanal.js
@@ -4,8 +4,8 @@ module.exports = function (sequelize, DataTypes) {
   return sequelize.define('plan_semanal', {
     id: {
       type: DataTypes.INTEGER(11),
-      allowNull: false,
       primaryKey: true,
+      autoIncrement: true,
     },
     nombre: {
       type: DataTypes.STRING(45),
