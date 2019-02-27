dc2e5c5ac668296f89102725560219e85ca6a3aa
valeri <valeritsert@gmail.com>
2019-02-27 09:31:45 +0400

    add comidas_recetas

---
+++ api/models/comidas_recetas.js
@@ -0,0 +1,46 @@
+/* jshint indent: 2 */
+
+module.exports = function (sequelize, DataTypes) {
+  return sequelize.define('comidas_recetas', {
+    comidas_id: {
+      type: DataTypes.INTEGER(11),
+      allowNull: false,
+    },
+    recetas_id: {
+      type: DataTypes.INTEGER(11),
+      allowNull: false,
+    },
+
+    // Inherit fields for recetas
+    fecha_creacion: {
+      type: DataTypes.STRING(15),
+      allowNull: true,
+    },
+    nombre_receta: {
+      type: DataTypes.STRING(45),
+      allowNull: true,
+    },
+    tipo_comida: {
+      type: DataTypes.STRING(15),
+      allowNull: true,
+    },
+    tipo_dieta: {
+      type: DataTypes.STRING(45),
+      allowNull: true,
+    },
+    tipo_receta: {
+      type: DataTypes.STRING(15),
+      allowNull: true,
+    },
+    publica_privada: {
+      type: DataTypes.STRING(1),
+      allowNull: true,
+    },
+    explicacion: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+  }, {
+    tableName: 'comidas_recetas',
+  });
+};
