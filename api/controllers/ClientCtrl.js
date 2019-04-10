2bfdeda19362e29e28c5da08fa78da4c8117ecb6
valeri <valeritsert@gmail.com>
2019-04-10 10:28:44 +0400

    add client controller

---
+++ api/controllers/ClientCtrl.js
@@ -0,0 +1,149 @@
+const { Op } = require('sequelize');
+
+const Client = require('../models').clientes;
+const User = require('../models').loginuser;
+const { getMinMax, getDistinct, queryAll } = require('./common');
+
+const { onError } = require('./error');
+
+const ClientCtrl = () => {
+  const bulkUpdate = async (req, res) => {
+    try {
+      await Promise.all(req.body.map((model) => Client.update(model, {
+        where: { id: model.id },
+      })));
+
+      const models = await Client.findAll({
+        where: {
+          id: {
+            $in: req.body.map((model) => model.id),
+          },
+        },
+      });
+
+      return res.send({
+        status: true,
+        data: models.map((model) => model.toJSON()),
+      });
+    } catch (e) {
+      return onError(req, res, e);
+    }
+  };
+
+  const update = async (req, res) => {
+    try {
+      const result = await Client.update(req.body, {
+        where: {
+          id: req.params.id,
+        },
+      });
+      if (result[0] === 0) { // Affected element count
+        return res.status(404).send({
+          status: false,
+          error: 'Client_not_found',
+        });
+      }
+
+      const data = await Client.find({
+        where: {
+          id: req.params.id,
+        },
+      });
+
+      return res.send({
+        status: true,
+        data: data.toJSON(),
+      });
+    } catch (e) {
+      return onError(req, res, e);
+    }
+  };
+
+  const deleteSingle = async (req, res) => {
+    try {
+      const model = await Client.find({
+        where: { id: req.params.id },
+      });
+
+      if (!model) {
+        return res.status(404).send({
+          status: false,
+          error: 'Client_not_found',
+        });
+      }
+      await model.destroy();
+      return res.send({
+        status: true,
+      });
+    } catch (err) {
+      return onError(req, res, err);
+    }
+  };
+
+  const deleteAll = async (req, res) => {
+    try {
+      await Client.destroy({
+        where: {},
+      });
+
+      return res.send({
+        status: true,
+      });
+    } catch (err) {
+      return onError(req, res, err);
+    }
+  };
+
+  const get = async (req, res) => {
+    try {
+      const model = await Client.find({
+        where: { id: req.params.id },
+        include: [{
+          model: ClientAlimentos,
+        }],
+      });
+
+      if (model) {
+        return res.send({
+          status: true,
+          data: model.toJSON(),
+        });
+      }
+      return res.status(404).send({
+        status: false,
+        error: 'Client_not_found',
+      });
+    } catch (err) {
+      return onError(req, res, err);
+    }
+  };
+
+  const create = async (req, res) => {
+    try {
+      const data = req.body;
+      
+      const model = await Client.build(data).save();
+
+      return res.send({
+        status: true,
+        data: { ...model.toJSON() },
+      });
+    } catch (err) {
+      return onError(req, res, err);
+    }
+  };
+
+  return {
+    create,
+    get,
+    getAll: queryAll(Client, { model: User }),
+    deleteAll,
+    bulkUpdate,
+    update,
+    deleteSingle,
+    getMinMax: getMinMax(Client),
+    getDistinct: getDistinct(Client),
+  };
+};
+
+module.exports = ClientController;
