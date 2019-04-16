ac5348d59adaac61c2e918b0a5eae4db38bf69d9
valeri <valeritsert@gmail.com>
2019-04-16 09:15:10 +0400

    allow original id null for recetas

--- api/models/models/recetas.js
+++ api/models/models/recetas.js
@@ -41,7 +41,7 @@ module.exports = function (sequelize, DataTypes) {
     },
     id_original: {
       type: DataTypes.INTEGER(11),
-      allowNull: false,
+      allowNull: true,
     },
   }, {
     tableName: 'recetas',
